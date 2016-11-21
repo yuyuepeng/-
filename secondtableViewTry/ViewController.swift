af8f0a8dfd36528a9772aa40d156cd2fce1b73f7
yuyuepeng <616250176@qq.com>
2016-11-21 11:46:38 +0800

    Initial Commit

---
+++ secondtableViewTry/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  secondtableViewTry
+//
+//  Created by 扶摇先生 on 16/11/21.
+//  Copyright © 2016年 扶摇先生. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ secondtableViewTryUITests/secondtableViewTryUITests.swift
@@ -0,0 +1,36 @@
+//
+//  secondtableViewTryUITests.swift
+//  secondtableViewTryUITests
+//
+//  Created by 扶摇先生 on 16/11/21.
+//  Copyright © 2016年 扶摇先生. All rights reserved.
+//
+
+import XCTest
+
+class secondtableViewTryUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
